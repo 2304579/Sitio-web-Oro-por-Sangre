812e5d855537d8027870117f1e72cabaa1556a8a
2304579 <23sp04579@cbslp.edu.mx>
2026-05-10 19:02:20 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 function show(id){
+
     document.querySelectorAll("section").forEach(sec=>{
         sec.classList.remove("active");
     });
+
     document.getElementById(id).classList.add("active");
+
+    window.scrollTo({
+        top:0,
+        behavior:"smooth"
+    });
+
 }
